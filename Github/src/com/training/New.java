6aa3233e1ee9125630b54ccee3dba27fee161b3a
suhailahamed <suhailahmed648@gmail.com>
2018-01-30 11:03:48 +0530

    commit

--- Github/src/com/training/New.java
+++ Github/src/com/training/New.java
@@ -5,7 +5,7 @@ public class New {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("New class");
-		System.out.println("New class class");
+		System.out.println("New class class class");
 	}
 
 }
